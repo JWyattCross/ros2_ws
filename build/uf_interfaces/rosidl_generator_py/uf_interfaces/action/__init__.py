b33c5e656a86c420fdd4374c03a85f503aa252ff
James Cross <jameswyattcross@hotmail.com>
2024-03-19 13:39:31 -0400

    fix build issues

--- build/uf_interfaces/rosidl_generator_py/uf_interfaces/action/__init__.py
+++
@@ -1,2 +0,0 @@
-from uf_interfaces.action._defend import Defend  # noqa: F401
-from uf_interfaces.action._herd import Herd  # noqa: F401
---
+++ src/test_herding_pkg/test_herding_pkg/test_herding_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from test_herding_pkg.')
+
+
+if __name__ == '__main__':
+    main()
