81ab37d4c734c55f5aeb1c322529b798aefc0142
James Cross <jameswyattcross@hotmail.com>
2024-03-21 14:41:35 -0400

    uncomment

--- src/herding_v1_package/herding_v1_package/herding_node.py
+++ src/herding_v1_package/herding_v1_package/herding_node.py
@@ -65,11 +65,11 @@ class HerdingNode(Node): #create package
 
     def agent1_pos_callback(self, msg): #get real positions and update sim. This runs whenever a postion is revieved to update our position array
         self.real_pos_agent1 = np.array([msg.pose.position.x, msg.pose.position.y])
-        #self.get_logger().info(f'Agent1 position:  [{msg.pose.position.x}, {msg.pose.position.y}]')
+        self.get_logger().info(f'Agent1 position:  [{msg.pose.position.x}, {msg.pose.position.y}]')
 
     def target1_pos_callback(self, msg):
         self.real_pos_target1 = np.array([msg.pose.position.x, msg.pose.position.y])
-        #self.get_logger().info(f'Target1 position: [{msg.pose.position.x}, {msg.pose.position.y}]')
+        self.get_logger().info(f'Target1 position: [{msg.pose.position.x}, {msg.pose.position.y}]')
 
     def update_motion(self):
         if self.real_pos_agent1 is None or self.real_pos_target1 is None:
