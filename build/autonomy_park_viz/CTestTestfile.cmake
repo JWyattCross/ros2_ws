f84288233a7e2d4e56847a404d612d16559f2fb3
James Cross <jameswyattcross@hotmail.com>
2025-01-07 12:46:28 -0500

    organization

--- build/autonomy_park_viz/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wyatt/ros2_ws/src/autonomy-park-air-team-autonomy_park_viz
-# Build directory: /home/wyatt/ros2_ws/build/autonomy_park_viz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
